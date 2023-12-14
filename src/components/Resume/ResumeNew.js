3a16ae9b82ab1b2be13ffd2a7d0af7402b4167bd
YassineDeriouch <yderouich14@gmail.com>
2023-12-14 09:56:22 +0000

    cv update

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -44,7 +44,7 @@ function ResumeNew() {
         </Row>
 
         <Row className="resume">
-          <Document file={pdf} className="d-flex justify-content-center">
+          <Document file={pdf_frenchVersion} className="d-flex justify-content-center">
             <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
           </Document>
         </Row>
