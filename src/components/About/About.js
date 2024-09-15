497087c1b9fb84893ebeb10bf7088040e0df2b39
YassineDeriouch <yderouich14@gmail.com>
2024-09-15 02:56:58 +0100

    skills title change

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -44,7 +44,7 @@ function About() {
           </Col>
         </Row>
         <h1 className="project-heading">
-          Professional <strong className="purple">Skills </strong>
+          Technical <strong className="purple">Skills </strong>
         </h1>
 
         <Techstack />
